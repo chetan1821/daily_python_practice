d094de5b58084afc861c5a8313cd9c4841226cc4
chetan1821 <cp7292367@gmail.com>
2026-01-18 22:22:05 +0530

    All

---
+++ patterns/numberpattern.py
@@ -0,0 +1,73 @@
+# row = 3
+# for i in range(row):
+#     for j in range(1,row+1):
+#         print(j,end="")
+#     print()
+
+# 123
+# 123
+# 123
+
+# row = 5
+# for i in range(row):
+#     for j in range(1,row+1):
+#         print(j,end="")
+#     print()
+
+# 12345
+# 12345
+# 12345
+# 12345
+# 12345
+
+# row = 5
+# for i in range(1,row+1):
+#     for j in range(1,i+1):
+#         print(i,end="")
+#     print()
+
+# 1
+# 22
+# 333
+# 4444
+# 55555
+
+# row = 5
+# for i in range(row):
+#     for j in range(i+1):
+#         print(j+1,end="")
+#     print()
+
+# 1
+# 12
+# 123
+# 1234
+# 12345
+
+# row = 6 
+# for i in range(1,row):
+#     print(i)
+# print()
+
+# 1
+# 2
+# 3
+# 4
+# 5
+
+row=5
+num=1
+for i in range(1,row+1):
+    for j in range(i):
+        print(num, end=" ")
+        num += 1
+    print()
+
+
+
+
+
+
+
+
+
---
+++ patterns/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+for i in range(row):
+    for j in range(1,row+1):
+        print(j,end="")
+    print()
